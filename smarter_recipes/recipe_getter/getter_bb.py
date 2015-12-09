93e3fdb1bb244cbe2c90a7fae69d604feb0c0748
Elena Palmer <c.elena.palmer@gmail.com>
2015-12-09 14:45:03 -0600

    Recipe Site Parser Update
    This commit updates the BB recipe parser. Next steps will be
    parallelizing the script to improve performance and abstracting
    the parser so that calling/crawling is separate from site-specific
    config/implementation.

--- smarter_recipes/recipe_getter/getter_bb.py
+++ smarter_recipes/recipe_getter/getter_bb.py
@@ -14,16 +14,40 @@ def fix_encoding(item):
         newtext = item.encode('utf-8', 'replace')
         return newtext
 
-html = urllib.urlopen('http://www.budgetbytes.com/2015/12/30-minute-posole/').read()
-recipe = BeautifulSoup(html)
-recipe = recipe.find_all('li', class_='ingredient')
-ingredients_list = []
-for item in recipe:
-    item = fix_encoding(item.text)
-    item = transform_ingredient(str(item))
-    ingredients_list.append(item)
+def get_ingredients(url):
+    html = urllib.urlopen(url).read()
+    recipe = BeautifulSoup(html)
+    recipe = recipe.find_all('li', class_='ingredient')
+    ingredients_list = []
+    for item in recipe:
+        item = fix_encoding(item.text)
+        item = transform_ingredient(str(item))
+        ingredients_list.append(item)
+    return ingredients_list
 
-print ingredients_list
+def get_urls():
+    html = urllib.urlopen('http://budgetbytes.com').read()
+    soup = BeautifulSoup(html)
+    dates = soup.find_all('time', class_='entry-time')
+    dates_list = []
+    for date in dates:
+        dates_list.append(str(date.text))
+    urls = soup.find_all('h2', class_='entry-title', limit=10)
+    recipe_urls = []
+    for url in urls:
+        for a in url:
+            recipe_urls.append(a["href"])
+    return recipe_urls
 
+def open_recipes():
+    recipe_urls = get_urls()
+    final_data = {}
+    for url in recipe_urls:
+        recipe_ingredients = get_ingredients(url)
+        final_data[url] = recipe_ingredients
+    print final_data
+
+
+open_recipes()
 
 
