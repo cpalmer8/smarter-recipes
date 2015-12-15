66995cdc87f54831169a5ad62ac85d3943e04f3b
Elena Palmer <c.elena.palmer@gmail.com>
2015-12-14 18:00:37 -0600

    Adding a second recipe website
    This commit adds a second recipe website to the scraping
    mechanism. This is still a prototype (hardcoded etc).
    Added to the todo is better data validation to strip whitespace
    and other junk data.

--- smarter_recipes/recipe_getter/getter_main.py
+++ smarter_recipes/recipe_getter/getter_main.py
@@ -1,8 +1,9 @@
 import getter_bb
-
+import getter_redbowls
 
 def main():
     getter_bb.open_recipes()
+    getter_redbowls.open_recipes()
 
 if __name__ == "__main__":
     main()
--- smarter_recipes/recipe_getter/getter_redbowls.py
+++ smarter_recipes/recipe_getter/getter_redbowls.py
@@ -3,10 +3,6 @@ import urllib
 import re
 import couchdb
 
-#budget bytes recipes come with dollar amounts, we want to remove those
-def transform_ingredient(ingredient):
-    ingredient = re.sub(r'[\$][0-9]*\.[0-9]*', "", ingredient)
-    return ingredient
 
 #fix encoding so unicode fractions won't break the script
 def fix_encoding(item):
@@ -24,9 +20,9 @@ def get_ingredients(url):
     recipe = recipe.find_all('li', class_='ingredient')
     ingredients_list = []
     for item in recipe:
-        item = fix_encoding(item.text)
-        item = transform_ingredient(str(item))
-        ingredients_list.append(item)
+        if (item.text):
+            item = fix_encoding(item.text)
+            ingredients_list.append(item)
     return ingredients_list
 
 #gets a list of the 'first' 10 urls, corresponding to updated blog posts
@@ -34,9 +30,9 @@ def get_ingredients(url):
 #ideally this would not be managed with a nested loop
 #also needs socket improvement/parallelization per cProfile analysis
 def get_urls():
-    html = urllib.urlopen('http://budgetbytes.com').read()
+    html = urllib.urlopen('http://tworedbowls.com').read()
     soup = BeautifulSoup(html)
-    urls = soup.find_all('h2', class_='entry-title', limit=10)
+    urls = soup.find_all('h1', class_='entry-title', limit=10)
     recipe_urls = []
     for url in urls:
         for a in url:
@@ -61,7 +57,6 @@ def open_recipes():
         if (recipe_ingredients):
             final_data[url] = recipe_ingredients
     insert_data(final_data)
-
 
 
 
