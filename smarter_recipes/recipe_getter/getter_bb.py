35fabfd54399daea8691aed50c0e6d9400a388d2
Elena Palmer <c.elena.palmer@gmail.com>
2015-12-15 10:26:23 -0600

    Using YAML For Config
    This commit begins to use YAML to configure the scraping script.

--- smarter_recipes/recipe_getter/getter_bb.py
+++ smarter_recipes/recipe_getter/getter_bb.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 import couchdb
+import yaml
 
 #budget bytes recipes come with dollar amounts, we want to remove those
 def transform_ingredient(ingredient):
@@ -18,10 +19,10 @@ def fix_encoding(item):
         return newtext
 
 #get array of ingredients from each url
-def get_ingredients(url):
+def get_ingredients(url, ingredient_selector, ingredient_class):
     html = urllib.urlopen(url).read()
     recipe = BeautifulSoup(html)
-    recipe = recipe.find_all('li', class_='ingredient')
+    recipe = recipe.find_all(ingredient_selector, class_=ingredient_class)
     ingredients_list = []
     for item in recipe:
         item = fix_encoding(item.text)
@@ -33,10 +34,10 @@ def get_ingredients(url):
 #past 10 = older posts on different parts of the page
 #ideally this would not be managed with a nested loop
 #also needs socket improvement/parallelization per cProfile analysis
-def get_urls():
-    html = urllib.urlopen('http://budgetbytes.com').read()
+def get_urls(url, link_selector, link_class):
+    html = urllib.urlopen(url).read()
     soup = BeautifulSoup(html)
-    urls = soup.find_all('h2', class_='entry-title', limit=10)
+    urls = soup.find_all(link_selector, class_=link_class, limit=10)
     recipe_urls = []
     for url in urls:
         for a in url:
@@ -52,16 +53,23 @@ def insert_data(final_data):
     doc = final_data
     db.save(doc)
 
+def ingest_yaml(key):
+    with open('recipe_config.yml', 'r') as cfg:
+        doc = yaml.load(cfg)
+        url = doc[key]['url']
+        link_selector = doc[key]['link_selector']
+        link_class = doc[key]['link_class']
+        ingredient_selector = doc[key]['ingredient_selector']
+        ingredient_class = doc[key]['ingredient_class']
+    open_recipes(url, link_selector, link_class, ingredient_selector, ingredient_class)
+
 #gets recipes & ingredients, puts them in a dict
-def open_recipes():
-    recipe_urls = get_urls()
+def open_recipes(url, link_selector, link_class, ingredient_selector, ingredient_class):
+    recipe_urls = get_urls(url, link_selector, link_class)
     final_data = {}
-    for url in recipe_urls:
-        recipe_ingredients = get_ingredients(url)
+    for recipe in recipe_urls:
+        recipe_ingredients = get_ingredients(recipe, ingredient_selector, ingredient_class)
         if (recipe_ingredients):
-            final_data[url] = recipe_ingredients
+            final_data[recipe] = recipe_ingredients
     insert_data(final_data)
-
-
-
 
--- smarter_recipes/recipe_getter/getter_main.py
+++ smarter_recipes/recipe_getter/getter_main.py
@@ -2,7 +2,7 @@ import getter_bb
 import getter_redbowls
 
 def main():
-    getter_bb.open_recipes()
+    getter_bb.ingest_yaml('budget_bytes')
     getter_redbowls.open_recipes()
 
 if __name__ == "__main__":
